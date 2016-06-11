bd302ba62b791d65e3886f490df498bb65a10868
Pedro de Lyra <pedrodelyra@gmail.com>
2016-06-10 22:56:41 -0300

    Add bootstrap to project

--- hurricane_truck/settings.py
+++ hurricane_truck/settings.py
@@ -53,10 +53,12 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'hurricane_truck.urls'
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_ROOT, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, "hurricane_truck/static"),
+]
