3bd4b04f488e8ff41949d68c938085551fe69ef9
Pedro de Lyra <pedrodelyra@gmail.com>
2016-06-14 09:48:23 -0300

    Add micropost model

--- microposts/models.py
+++ microposts/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Micropost(models.Model):
+	content = models.CharField(max_length=150)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	pub_date = models.DateTimeField('date published')
+
+	def __str__(self):
+		return self.content
